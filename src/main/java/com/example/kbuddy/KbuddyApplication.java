c89d1c21b0b8d833f4e8f9bbdd20e92e4e001974
Ko Jea Min <onecombo37@gmail.com>
2026-08-09 05:00:37 +0900

    chore: initialize backend project structure

---
+++ src/main/java/com/example/kbuddy/KbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbuddyApplication.class, args);
+	}
+
+}
